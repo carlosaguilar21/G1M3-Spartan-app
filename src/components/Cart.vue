342a1bbf00a53306a32a5e2d6152b533fab596b2
carlosaguilar21 <75148179+carlosaguilar21@users.noreply.github.com>
2020-12-17 12:19:34 -0500

    second commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <v-container>
+
+      <p class="display-3 font-weight-light	text-center pa-4">CARRITO DE COMPRA</p>
+      <v-row>
+        <v-col :cols="12" md="9" sm="12" >
+          <v-simple-table>
+            <template v-slot:default>
+              <thead>
+              <tr>
+                <th class="text-center">PRODUCTO</th>
+                <th class="text-center">PRECIO</th>
+                <th class="text-center">CANTIDAD</th>
+                <th class="text-center">TOTAL</th>
+                <th class="text-center"></th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>
+                  <v-list-item
+                  key="1"
+                  @click=""
+                >
+                  <v-list-item-avatar>
+                    <v-img :src="require('../assets/img/shop/1.jpg')"></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title >Producto 1</v-list-item-title>
+                    <v-list-item-subtitle>ARTICULO</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item></td>
+                <td>$400000.00</td>
+                <td>
+                  <v-text-field
+                    class="pt-10"
+                    label="Outlined"
+                    style="width: 80px;"
+                    single-line
+                    outlined
+                    value="2"
+                    type="number"
+                  ></v-text-field>
+                </td>
+                <td>$70000.00</td>
+                <td><a>X</a></td>
+              </tr>
+              <tr>
+                <td>
+                  <v-list-item
+                  key="1"
+                  @click=""
+                >
+                  <v-list-item-avatar>
+                    <v-img :src="require('../assets/img/shop/2.jpg')"></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title >Producto 2</v-list-item-title>
+                    <v-list-item-subtitle>Articulo</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item></td>
+                <td>$800000.00</td>
+                <td>
+                  <v-text-field
+                    class="pt-10"
+                    label="Outlined"
+                    style="width: 80px;"
+                    single-line
+                    outlined
+                    value="2"
+                    type="number"
+                  ></v-text-field>
+                </td>
+                <td>$100000.00</td>
+                <td><a>X</a></td>
+              </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+        <v-col :cols="12" md="3" sm="12" style="background-color: lightgray">
+          <p class="headline">RESUMEN DE ORDEN</p>
+          <p class="overline"> El envio y costos adicionales son calculados en base a los valores ingresados.
+          </p>
+          <v-simple-table>
+            <template v-slot:default>
+              <tbody>
+              <tr>
+                <td>Order Subtotal</td>
+                <td class="text-right" style="width: 50px;">$1600000.00</td>
+              </tr>
+              <tr>
+                <td>Cargos de Envio</td>
+                <td class="text-right" style="width: 50px;">$100000.00</td>
+              </tr>
+              <tr>
+                <td>Impuesto</td>
+                <td class="text-right" style="width: 50px;">$5000.00</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td class="text-right" style="width: 50px;"><b>$17500000.00</b></td>
+              </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+          <div class="text-center">
+            <v-btn class="primary white--text mt-5" outlined>PROCEED TO PAY</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-card  class="accent" >
+      <v-container>
+        <v-row no-gutters>
+          <v-col class="col-12 col-md-4 col-sm-12">
+            <v-row >
+              <v-col class="col-12 col-sm-3 pr-4 hidden-sm-only" align="right">
+                <v-icon class="display-2">mdi-truck</v-icon>
+              </v-col>
+              <v-col class="col-12 col-sm-9 pr-4">
+                <h3 class="font-weight-light">FREE SHIPPING & RETURN</h3>
+                <p class="font-weight-thin">Free Shipping over $300</p>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col class="col-12 col-md-4 col-sm-12">
+            <v-row >
+              <v-col class="col-12 col-sm-3 pr-4" align="right">
+                <v-icon class="display-2">mdi-cash-usd</v-icon>
+              </v-col>
+              <v-col  class="col-12 col-sm-9 pr-4">
+                <h3 class="font-weight-light">MONEY BACK GUARANTEE</h3>
+                <p class="font-weight-thin">30 Days Money Back Guarantee</p>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col class="col-12 col-md-4 col-sm-12">
+            <v-row>
+              <v-col class="col-12 col-sm-3 pr-4" align="right">
+                <v-icon class="display-2">mdi-headset</v-icon>
+              </v-col>
+              <v-col  class="col-12 col-sm-9 pr-4">
+                <h3 class="font-weight-light">020-800-456-747</h3>
+                <p class="font-weight-thin">24/7 Available Support</p>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+<script>
+    export default {
+        data: () => ({
+            rating: 4.5,
+            breadcrums: [
+                {
+                    text: 'Home',
+                    disabled: false,
+                    href: 'breadcrumbs_home',
+                },
+                {
+                    text: 'Clothing',
+                    disabled: false,
+                    href: 'breadcrumbs_clothing',
+                },
+                {
+                    text: 'T-Shirts',
+                    disabled: true,
+                    href: 'breadcrumbs_shirts',
+                },
+            ],
+        })
+    }
+</script>
+
